d3c967fb949b016431d68a146192303b38cf12bd
poojapol01 <polpooja05@gmail.com>
2023-01-04 00:17:18 +0530

    Added Racket Movements with Bounce

---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+    public BallMovement ballMovement;
+    float x;
+
+    void BounceFromRacket(Collision2D c)
+    {
+        Vector3 ballPosition = this.transform.position;
+        Vector3 racketPosition = c.gameObject.transform.position;
+
+        float racketHeight = c.collider.bounds.size.y;
+
+        if (c.gameObject.name == "RacketPlayer1")
+        {
+            x = 1;
+        }
+        if (c.gameObject.name == "RacketPlayer2")
+        {
+            x = -1;
+        }
+
+        float y = (ballPosition.y - racketPosition.y) / racketHeight;
+        this.ballMovement.IncreaseHitCounter();
+        this.ballMovement.MoveBall(new Vector2(x, y));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "RacketPlayer1" || collision.gameObject.name == "RacketPlayer2")
+        {
+            this.BounceFromRacket(collision);
+        }
+        else if(collision.gameObject.name == "WallLeft")
+        {
+            Debug.Log("Collision with WallLeft");
+        }
+
+        else if (collision.gameObject.name == "WallRight")
+        {
+            Debug.Log("Collision with WallRight");
+        }
+    }
+
+}
